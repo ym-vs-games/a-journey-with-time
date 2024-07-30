bdf361dbfc85ff2125f56ce76cf870abb68f8b33
ym-vs-games <ymiah3498@gmail.com>
2024-07-30 15:39:14 +0100

    player movement and camera control

---
+++ Assets/yahya/scripts/camera controls/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    [SerializeField] private Transform Player; 
+    [SerializeField] private float FollowSpeed; 
+    private Vector3 Offset; 
+
+    void Start()
+    {
+        Offset = transform.position - Player.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 DesiredPosition = Player.position + Offset;
+        transform.position = Vector3.Lerp(transform.position, DesiredPosition, FollowSpeed * Time.deltaTime);
+    }
+}
